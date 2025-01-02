3639cc00496ca1383b781db798ba62baf149b2c2
Sergio Castañeiras Morales <sergiocastaneirasmorales@gmail.com>
2025-01-02 20:21:55 +0100

    experimenting with picos

---
+++ Scripts/SquareRootMeasurement.py
@@ -0,0 +1,74 @@
+########################################################################################################
+#                                                                                                      #
+#  Project:  Physics TFG                                                                               #
+#  Author:   Sergio Castañeiras Morales                                                                #
+#  Date:     02/01/2025                                                                                #
+#  Purpose:  Testing the Square Root Measurement                                                       #
+#                                                                                                      #
+########################################################################################################
+import numpy
+import picos
+
+# defining sigma_y
+sigma_y=picos.Constant([[0, -1j],[1j,0]])
+
+# Show sigma_y
+print("Sigma_y = ",sigma_y)
+
+# print sigma_y dagger,
+print("Sigma_y dagger = ",sigma_y.H)
+
+#Creating a function that tells us weather a matrix is hermitian or not 
+def Hermitian(matrix):
+    return numpy.allclose(matrix.value, matrix.H.value)
+
+# Is 'sigma_y' hermitian
+print("Is sigma_y hermitian? ", Hermitian(sigma_y))
+
+# Lets create an hermitian we start by defining a non hermitian matrix '_P'
+_P = picos.Constant([
+[ 1 -1j, 2 +2j, 1 ],
+[ 3j, -2j, -1 -1j],
+[ 1 +2j, -0.5+1j, 1.5 ]])
+
+print("Is '_P' hermitian? ", Hermitian(_P))
+
+# now we construct P as _P*_P^dagger which ensures that P is hermitian
+
+P = (_P*_P.H).renamed("P")
+
+print("Is 'P' hermitian? ", Hermitian(P))
+
+# We do the same procedure with a Q matrix
+
+_Q = picos.Constant([
+[-1 -2j, 2j, 1.5 ],
+[ 1 +2j, -2j, 2.0-3j],
+[ 1 +2j, -1 +1j, 1 +4j]])
+Q = (_Q*_Q.H).renamed("Q")
+
+print("Is '_Q' hermitian? ", Hermitian(_P))
+
+print("Is 'Q' hermitian? ", Hermitian(P))
+
+"""
+# Define the problem.
+F = picos.Problem()
+Z = picos.ComplexVariable("Z", P.shape)
+F.set_objective("max", 0.5*picos.trace(Z + Z.H))
+F.add_constraint(((P & Z) // (Z.H & Q)) >> 0)
+print(F)
+# Solve the problem.
+F.solve(solver = "cvxopt")
+print("\nOptimal value:", round(F, 4))
+print("Optimal Z:", Z.value, sep="\n")
+# Also compute the fidelity via NumPy for comparison.
+PP = numpy.matrix(P.value)
+QQ = numpy.matrix(Q.value)
+S,U = numpy.linalg.eig(PP)
+sqP = U * numpy.diag([s**0.5 for s in S]) * U.H # Square root of P.
+S,U = numpy.linalg.eig(QQ)
+sqQ = U * numpy.diag([s**0.5 for s in S]) * U.H # Square root of Q.
+Fnp = sum(numpy.linalg.svd(sqP * sqQ)[1]) # Trace-norm of sqrt(P)·sqrt(Q).
+print("Fidelity F(P,Q) computed by NumPy:", round(Fnp, 4))
+"""
